ac0f13c2f65b963c299ec174c950cdfd40028567
Cesare Gilento <ftcli@proton.me>
2025-04-06 10:03:52 +0200

    Refactor init command to improve styles mapping file initialization and handling

--- src/foundrytools_cli_ng/commands/assistant/cli.py
+++ src/foundrytools_cli_ng/commands/assistant/cli.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import click
 
-from foundrytools_cli_ng.commands.assistant.styles_mapping import StylesMappingHandler
+from foundrytools_cli_ng.commands.assistant.styles_mapping import (
+    StylesMappingHandler,
+)
 from foundrytools_cli_ng.utils.logger import logger
 
 cli = click.Group(help="Utilities for fixing the ``name`` table.")
@@ -10,11 +12,16 @@ cli = click.Group(help="Utilities for fixing the ``name`` table.")
 
 @cli.command("init")
 @click.argument("input_path", type=click.Path(exists=True, resolve_path=True, path_type=Path))
-def init_assistant(input_path: Path) -> None:
+def init_mapping(input_path: Path) -> None:
     """
     Initialize the name table of a font.
     """
-    styles_mapping = StylesMappingHandler(input_path)
-    styles_mapping.data["weights"][1200] = ["Blackz", "Blackest"]
-    styles_mapping.save_to_file(styles_mapping.data)
-    logger.info("Styles mapping file initialized.")
+    styles_mapping_file = StylesMappingHandler.get_file_path(input_path)
+    if styles_mapping_file.exists():
+        logger.warning(f"Styles mapping file already exists at {styles_mapping_file}")
+        click.confirm("Do you want to overwrite the existing styles mapping file?", abort=True)
+
+    styles_mapping_handler = StylesMappingHandler(input_path)
+    styles_mapping_handler.reset_defaults()
+
+    logger.info(f"Styles mapping file created at {styles_mapping_file}")
